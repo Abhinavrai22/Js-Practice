f2e856ffdf1f5db9e0506037afe13bcfa0dfb69b
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-21 12:23:37 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -13,10 +13,10 @@ console.log(valueInNumber);
 // true => 1; false => 0;
 
 
-let isLoggedIn ="Abhinav"
+// let isLoggedIn ="Abhinav"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0=> false
@@ -24,11 +24,38 @@ console.log(booleanIsLoggedIn);
 // "Abhinav" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//---------- operation ----------------
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1= "hello"
+let str2 = " Abhinav"
+console.log(str1+str2);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+
+
+
 
 
 
