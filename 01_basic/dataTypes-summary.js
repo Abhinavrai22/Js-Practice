e0a315f5e18dc020ddda38615a1e1dd2827de6b3
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-21 13:08:09 +0000

    summary

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,35 @@
+// Data types 
+// 2 Types of data types 
+// Primitive and non-primitive
+
+// Primitive
+
+// number => number
+// string => string
+// bigInt => number
+// boolean => boolean
+// null => object
+// undefined => undefined
+// symbol => symbol
+
+
+// non-Primitive
+
+// object => object
+// array => object
+// Function => object Function
+
+
+const isLoggedIn = false
+const outsidetemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(anotherId);
+
+console.log(id===anotherId);
+
+
