e5f713d61a2e45c322983266d24f790d34414baa
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-21 10:32:15 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => nan
+// true => 1; false => 0;
+
+
+let isLoggedIn ="Abhinav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0=> false
+// "" => false
+// "Abhinav" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
