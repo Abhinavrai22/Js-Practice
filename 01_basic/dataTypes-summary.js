3a012cc3be68ca088d0d0ede1b41bc546a0e8ebb
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-21 13:31:33 +0000

    memory

--- 01_basic/dataTypes-summary.js
+++ 01_basic/dataTypes-summary.js
@@ -33,3 +33,42 @@ console.log(anotherId);
 console.log(id===anotherId);
 
 
+
+// ----------------------------------------------Memory-----------------------------------------------
+
+
+// There are two type of memory in the javascript 
+// Stack and Heap 
+
+
+// Stack(Primitive); Heap(Non-Primitive)
+
+
+let name = "Abhinav RAi"
+
+let nickName = name
+
+nickName = "Abhi"
+
+
+
+console.log(name); // here all the data which is stored in the stack used copy of original data to make changes
+console.log(nickName);// so the value of original is not changed 
+
+let userOne ={
+    email: "abhinavrai2014@gmail.com",
+    age: 24
+}
+
+let userTwo = userOne
+
+userTwo.email = "abhi022rai@gmail.com"
+
+console.log(userOne.email);// while in heap the memory gives the original data to change the value
+console.log(userTwo.email);// so here both value is changed because of original data is manipulated 
+
+
+
+
+
+
