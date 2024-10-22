6f296210586665a716bd20a4a9fb76afe6abd137
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-22 11:55:14 +0000

    num and math

---
+++ 01_basic/06_num_maths.js
@@ -0,0 +1,49 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(2));
+
+
+
+// ------------------------------------Maths--------------------------------------------
+
+
+// console.log(Math.abs(-8));
+
+// console.log(Math.round(5.6));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.4));
+
+// console.log(Math.min(5,6,2,98,0));
+
+// console.log(Math.max(1, 56, 99,100));
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
