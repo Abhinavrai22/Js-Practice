e7e440793f8f0bf09eba5adcb79465ec70aade35
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-22 12:20:38 +0000

    dates

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDates = new Date()
+
+console.log(myDates.toString());
+console.log(myDates.toDateString());
+console.log(myDates.toISOString());
+console.log(myDates.toJSON());
+console.log(myDates.toLocaleDateString());
+
