03bb4e4b0c1674718163829d404899cd911a5121
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-10-22 13:22:23 +0000

    Arrays part1

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,27 @@
+const myArr =[2,3,5,8,9,10]
+const myHeros =["Nagraj", "SpiderMan"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArr[1]);
+// myArr.push(89)
+// myArr.push(99)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+// console.log(myArr.includes(10));
+
+// console.log(myArr.indexOf(10));
+
+const myA1 =[0,1,2,3,4,5,6,7]
+
+console.log(myA1);
+
+console.log(myA1.slice(1,5));
+console.log(`A ${myA1}`);
+
+console.log(myA1.splice(1,5));
+console.log(myA1);
+
+
+
